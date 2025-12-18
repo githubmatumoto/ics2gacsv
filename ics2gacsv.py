f110631a821fe2d1f572dc46c7514ec5d7f03739
Ryuji MATSUMOTO <matsumoto.ryuji959@mail.kyutech.jp>
2025-12-18 15:58:47 +0900

    add copyright.

--- ics2gacsv.py
+++ ics2gacsv.py
@@ -1,53 +1,64 @@
 #!/usr/bin/env python3
 # -*- python -*-
 # -*- coding: utf-8 -*-
+#
+# Copyright (c) 2025 MATSUMOTO Ryuji.
+# License: Apache License 2.0
+#
 import os
 import sys
 import getopt
 import re
 import libics2gacsv
 
-__doc__=f"""ICS(iCalendar)をCSVに変換する。CSVの出力形式はGaroonと同じ。
+__doc__=f"""ICS(iCalendar)をCSVに変換。CSVの出力形式はGaroonとほぼ同じ。
 
 使用方法:
    $ python3 {sys.argv[0]} [-hubstoOdpmT] 期間 入力.ics 出力.csv
 
-   例
-   $ python3 {sys.argv[0]} 202512 calendar.ics schedules.csv
+   期間を指定して出力する例:
+   $ python3 {sys.argv[0]} 202512 calendar.ics schedules202512.csv
    $ python3 {sys.argv[0]} guess calendar.ics schedules202509.csv
+   $ python3 {sys.argv[0]} all calendar.ics schedules-all.csv
+
+   文字コードをUTF-8にする例(-u):
+   $ python3 {sys.argv[0]} -u all calendar.ics schedules-utf8.csv
 
 必須引数:
   「期間」: CSVの出力年月の期間指定。有効範囲は2000年1月から2099年
-   12月まで。
+   12月まで。西暦4桁+月2桁の合計6桁の数字で指定します。
 
    例えばCSVに2025年8月分を出力する場合は「202508」と指定する。
 
    特殊な値として、「all」もしくは「0」だと全期間を変換する。
   「guessin」だと入力ファイル名から期間を推測する。
   「guess」だと出力ファイル名から期間を推測する。例えば出力ファイル名が
-  「schedules.csv」なら2025年9月と推測する。
+  「schedules202509.csv」なら2025年9月と推測する。
 
    期間指定を行った場合、月末のスケジュールで月を超えてる場合は翌月分
    も含まれます。例えば11月30日23:00に開始で12月1日02:00に終了の場合は、
    11月分に12月1日02:00終了のスケジュールが入ります。12月分には入りま
    せん。
 
-「入力.ics」: 変換元のICSファイル名を指定。「stdin」を指定すると標準入力。
-    ICSファイルは規格で文字コードがUTF-8と決まってます。そのため、必ず
-    UTF-8のICSファイルを指定してください。
+
+「入力.ics」: 変換元のICSファイル名を指定。「stdin」を指定すると標準入
+    力。ICSファイルは規格(RFC5545)で文字コードがUTF-8と決まってます。
+    そのため、必ずUTF-8のICSファイルを指定してください。
 
     ※対応ICSファイル:
        Cybozu Garoon(Version 5.0.2)
        Web版 Outlook
        Windowsアプリ版 Outlook(classic)
-       いずれも2025年9月から12月ごろに生成されたICSファイルの出力で確認。
+    いずれも2025年9月から12月ごろに生成されたICSファイルの出力で確認。
 
-「出力.csv」: 変換先のCSVファイル名を指定。「stdout」を指定すると標準出力。
-    文字コードのdefaultはShiftJIS。
+「出力.csv」: 変換先のCSVファイル名を指定。CSVの出力形式はGaroonとほぼ
+    同じ形式で出力します。「stdout」を指定すると標準出力。
+    GaroonのCSVの文字コードのdefaultはShiftJISです。
 
 オプション引数:
 
--h : ヘルプを出力する。
+-h : ヘルプを出力する。ヘルプ画面の停止はアルファベットの「q」を押して
+     ください。
 
 -u : CSV出力の文字コードをUTF-8に変更する。defaultはShiftJIS。
     Garoonの生成するCSVのデフォルトの文字列はShiftJIS
@@ -56,27 +67,45 @@ __doc__=f"""ICS(iCalendar)をCSVに変換する。CSVの出力形式はGaroonと
     ある。採用するTimeZoneを指定する。日本標準時なら -T"Asia/Tokyo"な
     どを定義すればよい。
 
-以下は滅多に利用しない引数です。開発者向け。
+    ※本オプションをWindows環境で使う場合はライブラリ「tzdata」をイン
+    ストールしてください。
 
--b : RRULE/EXDATEのバグフィックスを無効にする。defaultは有効。
-    ※詳細は関数bug_fix_rrule()をみよ。
+=====================================================================
 
--s : ICSのsummaryの分割を無効にする。defaultは有効。分割するとCSVの
-   「予定」/「予定詳細」がGaroonと同じ形式になる。
+特殊なオプション引数:
+
+以降で述べる引数は多くの場合は指定する必要は無いです。
+
+タイトル(SUMMARY)の分割関係:
+
+-s : ICSのsummaryの分割を無効にする。defaultは有効であり分割します。
+    分割するとCSVの「予定」/「予定詳細」がGaroonと同じ形式になる。
+
     ※詳細は関数split_garoon_style_summary()をみよ。
 
+-m : ICSのsummaryの分割で作者用の修正。defaultは無効。
+    ※詳細は関数split_garoon_style_summary()をみよ。
+
+時刻の表記関係:
+
+スケジュールで「終日スケジュール」関連は取扱いに細心の注意が必要です。
+詳細 misc/TECH-MEMO.txt に詳細に記載してます。
+
 -t : CSVの出力時刻にTimeZone情報を表示する。
+    ICSファイルにTimeZone情報が含まれていた場合、TimeZoneを表示する。
+    defaultでは表示しない。
 
-   default(TimeZone情報なし)/Garoonと同じ:
+    default(もしくはTimeZone情報なし):
         12:34:55
         12:34:55
 
-   オプション「-t」指定(TimeZone情報あり):
+    オプション「-t」指定(TimeZone情報あり):
         12:34:55+09:00
         12:34:55-03:00
 
     ※TimeZoneが複数定義が行われてる場合は正常に動作しない。
-    ※詳細は関数ics_time_to_csv()をみよ。
+    ※Garoonが生成するICSファイルにはTimeZone情報が無い。
+    ※詳細は関数ics_parts_to_csv_time()をみよ。
 
 -o : 終日スケジュール(時刻なし)のCSV出力形式を終日スケジュール
      内部表現(時刻あり)「0:00開始翌日0:00終了」とする。
@@ -94,7 +123,7 @@ __doc__=f"""ICS(iCalendar)をCSVに変換する。CSVの出力形式はGaroonと
 
     ※オプション「-g」と比較してください。
     ※Outlook(classic)はこの出力形式になります。
-    ※詳細は関数ics_time_to_csv()をみよ。
+    ※詳細は関数ics_parts_to_csv_time()および misc/TECH-MEMO.txt をみよ。
 
 -g : CSVの終日スケジュール(時刻あり)の出力形式を
     終日スケジュール内部表現(時刻なし1)とする。
@@ -111,30 +140,29 @@ __doc__=f"""ICS(iCalendar)をCSVに変換する。CSVの出力形式はGaroonと
       "2025/09/05","","2025/09/06","" (48時間の終日スケジュール)
 
     ※オプション「-g」と「-o」が同時指定された場合は「-g」が優先される。
-    ※詳細は関数ics_time_to_csv()をみよ。
+    ※詳細は関数ics_parts_to_csv_time()および misc/TECH-MEMO.txt をみよ。
 
+メモ欄(DESCRIPTION)関係:
 
 -d : ICSメモ欄(description)の4行目以降を消してCSVに出力する。
     defaultでは消さない。
 
     ※詳細は関数modify_description()をみよ。
 
--p : defaultではICSメモ欄(description)のTeamsの会議インフォメーションを消
+-p : defaultではICSメモ欄(DESCRIPTION)のTeamsの会議インフォメーションを消
      してCSVに出力する。パスワードが入ってるため。
 
-    本オプションを指定すると、本気が無効になり、Teamsの会議インフォメー
-    ションを消さない。
+    本オプションを指定すると、本機能が無効になり、Teamsの会議インフォ
+    メーションを消さない。
 
     ※詳細は関数modify_description()をみよ。
 
--m : ICSのsummaryの分割で作者用の修正。defaultは無効。
-
-    ※詳細は関数ics_time_to_csv()をみよ
-
--r : タイトル(SUMMARY)やメモ欄(description)の最後の改行や空白を除去する。
+-r : タイトル(SUMMARY)やメモ欄(DESCRIPTION)の最後の改行や空白を除去する。
     defaultでは除去しない。
 
     ※詳細は関数modify_description()をみよ。
+
+繰返しスケジュールの一部上書き(RECURRENCE_ID)関係:
 
 -w : 繰返しスケジュール(RRULE)の一部上書き(RECURRENCE_ID)を行った場合
      であっても、ICS上は上書きされる前の情報が残っている場合がある。
@@ -144,20 +172,38 @@ __doc__=f"""ICS(iCalendar)をCSVに変換する。CSVの出力形式はGaroonと
     たあとに、水曜日を修正し「昼食(外食)」とした場合、内部的には水曜日
     のスケジュールは「昼食(社食)」が残っている。
 
-    「-w」を指定すると一部修正する前のスケジュールも表示される。ただし
-     上書きを行ったスケジュールと区別するため、Summaryにprefixとして
-     「Hidden: 」が挿入される。具体的には水曜は「昼食(社食)」と
-     「Hidden: 昼食(社食)」となる。
+    「-w」を指定すると一部修正する前のスケジュールも表示する。
 
-    ※詳細はRFC5545のRECURRENCE_ID参照。
+     ただし上書きを行ったスケジュールと区別するため、Summaryにprefixと
+     して 「Hidden: 」が挿入される。
+
+     具体的には水曜は「昼食(社食)」と「Hidden: 昼食(社食)」となる。
+
+     ※詳細はRFC5545のRECURRENCE_ID参照。
 
 -x : RFC5545のRECURRENCE_ID関連の処理を一切おこなわない。
+
+バグ対策:
+
+-b : RRULE/EXDATEのバグフィックスを無効にする。defaultは有効。
+    ※詳細は関数bug_fix_rrule()および misc/TECH-MEMO.txt をみよ。
+
+=====================================================================
+
+ライセンス:
+Apache License 2.0
+
+配布元:
+{libics2gacsv.G_HAIFU_URL}
+{libics2gacsv.G_GITHUB_URL}
+
+修正履歴およびKnown bugsは misc/CHANGELOG.md 参照ください。
 """
 
 ########################################
 
-def myhelp():
-    help(EXEC_FILENAME)
+def __myhelp(fname):
+    help(fname)
     help("libics2gacsv")
     sys.exit()
 
@@ -170,14 +216,13 @@ if __name__ == '__main__':
     #CSVを出力する期間指定。
     TIMERANGE=0
 
-    EXEC_FILENAME=os.path.basename(__file__)
-    EXEC_FILENAME = re.sub(r'\.py$', "", EXEC_FILENAME)
+    exec_filename = os.path.basename(__file__)
+    exec_filename = re.sub(r'\.py$', "", exec_filename)
     opts, argv = getopt.getopt(sys.argv[1:], 'ubtsogdpmrwxhT:')
 
     try:
         for o, a in opts:
             if o == "-u":
-                libics2gacsv.flag_output_sjis = False
                 libics2gacsv.G_CSV_ENCODING = "utf-8"
             elif o == "-b":
                 libics2gacsv.flag_rrule_bugfix = False
@@ -204,7 +249,7 @@ if __name__ == '__main__':
             elif o == "-T":
                 libics2gacsv.G_OVERRIDE_TIMEZONE=a
             elif o == "-h":
-                myhelp()
+                __myhelp(exec_filename)
 
         if(len(argv)) < 3:
             raise ValueError("ERROR: 引数が足りません。")
@@ -243,23 +288,6 @@ if __name__ == '__main__':
     #time.tzset()
 
     #デバグ用のコード。代入したUIDのログが実行中に表示される。
-    #"040000008200E00074C5B7101A82E008000000009D4E4C179032DC0100000000000000001000000063C080275F56084E894FAE49A405DBE5"
-    #"040000008200E00074C5B7101A82E0080000000075B788729632DC010000000000000000100000009C20D09258570E4199EAC7CC0A14E046"
-    #libics2gacsv.G_DEBUG_UID="040000008200E00074C5B7101A82E0080000000037B5593475E5DB01000000000000000010000000A84100485775264A8625AC3F583EE7DA"
-    #libics2gacsv.G_DEBUG_UID="040000008200E00074C5B7101A82E008000000008B68E9A03037DC01000000000000000010000000D09E4FC835027B4F9060B3AA6DC5663B"
-    #コンマバグ
-    #libics2gacsv.G_DEBUG_UID="040000008200E00074C5B7101A82E00800000000C7B9368F2B37DC010000000000000000100000009159EF516B18E440A96C51D9D59FE41B"
-    #SUMMARY:TEST:10:火曜繰返し/終日/修正なし
-    #libics2gacsv.G_DEBUG_UID="040000008200E00074C5B7101A82E008000000006DBCF1032D37DC0100000000000000001000000071F28C928A4C3C409D0BE645A84E1E53"
-    #SUMMARY:TEST:11:月曜繰返し/終日/各種修正
-    #libics2gacsv.G_DEBUG_UID="040000008200E00074C5B7101A82E00800000000F1A5FF7F2D37DC0100000000000000001000000025716986E414A3469FED03DB7E61D271"
-    #SUMMARY:TEST:14:日曜繰返し(2)
-    #libics2gacsv.G_DEBUG_UID="040000008200E00074C5B7101A82E00800000000A683CBD92E37DC01000000000000000010000000541508FB10D07141901D6435A6F53411"
-    #Outloop(classic)
-    #libics2gacsv.G_DEBUG_UID="040000008200E00074C5B7101A82E00800000000CC3EC54B791CDC01000000000000000010000000933CF9EBB619DB418E6970F8C5972D61"
-    #libics2gacsv.flag_enable_recurrence_id_bug = True
-    #libics2gacsv.G_DEBUG_UID="040000008200E00074C5B7101A82E008000000004B76B8B78041DC0100000000000000001000000088A64D130C2329498F868EDE9CD69585"
-    #libics2gacsv.G_DEBUG_UID="040000008200E00074C5B7101A82E008000000006CCEC96E8141DC010000000000000000100000004561ADA64A21AC41A3768F73EB733CD5"
-    #libics2gacsv.G_DEBUG_UID="040000008200E00074C5B7101A82E008000000009C50BA40343FDC010000000000000000100000006134A510085D4C47BEB6645801704B78"
+    #libics2gacsv.G_DEBUG_UID="UIDを指定する"
     libics2gacsv.ics2csv(INPUT_ICS_FILENAME, OUTPUT_CSV_FILENAME, TIMERANGE)
 #End of main()
